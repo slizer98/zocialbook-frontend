9e1e5874bcdfe8bf605a0de158260fa75bbff156
slizer98 <erick.m.villa.r@gmail.com>
2024-02-16 17:00:31 -0600

    iniciando elegir avatar(incompleto)

---
+++ src/components/profile/ModalAvatars.vue
@@ -0,0 +1,64 @@
+<script setup>
+  import { ref } from 'vue'
+  import avartar1 from '@/assets/avatars/avatar1.png'
+  import avartar2 from '@/assets/avatars/avatar2.png'
+  import avartar3 from '@/assets/avatars/avatar3.png'
+  import avartar4 from '@/assets/avatars/avatar4.png'
+  import avartar5 from '@/assets/avatars/avatar5.png'
+  import avartar6 from '@/assets/avatars/avatar6.png'
+  import avartar7 from '@/assets/avatars/avatar7.png'
+  import avartar8 from '@/assets/avatars/avatar8.png'
+  import avartar9 from '@/assets/avatars/avatar9.png'
+
+  defineProps(['showAvatars'])
+  defineEmits(['toggleModalAvatars'])
+
+  const avatars = [
+    avartar1,
+    avartar2,
+    avartar3,
+    avartar4,
+    avartar5,
+    avartar6,
+    avartar7,
+    avartar8,
+    avartar9
+  ]
+
+  const selectedAvatar = (avatar) => {
+    console.log(avatar)
+  }
+  
+</script>
+
+<template>
+  <section 
+    v-if="showAvatars"
+    class="fixed inset-0   bg-black bg-opacity-50 flex items-center justify-center z-0"
+    @click="$emit('toggleModalAvatars')"
+  >
+    <div 
+      class=" w-5/6 sm:w-4/6 md:w-[60%] lg:w-[45%] snap-y h-3/4 p-2 sm:p-4 z-10  bg-white sm:h-auto shadow-xl rounded-lg relative"
+      @click.stop
+    >
+      <button 
+        class="absolute top-2 right-2 text-2xl z-10" 
+        @click="$emit('toggleModalAvatars')"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" style="fill: rgb(190, 136, 136);transform: ;msFilter:;"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
+      </button>
+      <h1 class="text-primary  font-bold text-base sm:text-xl mb-2">Selecciona un avatar</h1>
+      <ul class="grid grid-cols sm:grid-cols-3  gap-4 transition-all">
+        <li  v-for="avatar in avatars" @click="selectedAvatar(avatar)"  class="w-full border border-gray-300 flex justify-center hover:shadow-lg hover:border-gray-400 hover:scale-105">
+          <img :src="avatar" :alt="avatar" class=" w-2/5 sm:w-3/4">
+        </li>
+      </ul>
+      <button  class="bg-primary p-2 mt-4 rounded-md text-gray-100">Guardar Avatar</button>
+    </div>
+  </section>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/profile/ModalOptions.vue
@@ -0,0 +1,69 @@
+<script setup>
+  import { ref } from 'vue'
+  import ModalAvatars from './ModalAvatars.vue'
+
+ defineProps(['modalActive'])
+  
+    const showAvatars = ref(false)
+  
+    const toggleModalAvatars = () => {
+      showAvatars.value = !showAvatars.value
+    }
+ 
+ 
+</script>
+
+<template>
+  <div             
+    class="modal  absolute -bottom-12 -right-32   flex flex-col justify-center items-center rounded-md"
+    :class="{ 'modal-active': modalActive, 'modal-inactive': !modalActive }"
+    @click.stop
+  >
+    <button class="p-2 bg-blur hover:bg-blur-hover rounded-md  w-36 shadow-lg">Ver Foto</button>
+    <button class="p-2 bg-blur hover:bg-blur-hover rounded-md  w-36 shadow-lg">Cambiar Foto</button>
+    <button @click="toggleModalAvatars" class="p-2 bg-blur hover:bg-blur-hover rounded-md  w-36 shadow-lg">Poner Avatar</button>
+  </div>
+<ModalAvatars v-if="showAvatars" :showAvatars="showAvatars" @toggleModalAvatars="toggleModalAvatars" />
+</template>
+
+<style  scoped>
+  
+.options {
+  animation: spin .5s linear ;;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(180deg);
+  }
+}
+
+.modal {
+  transition: clip-path .5s ease-in-out;
+}
+.bg-blur{
+  background-color: rgba(159, 243, 194, 0.5);
+  backdrop-filter: blur(5px);
+  transition: all 0.3s ease-in-out;
+}
+.bg-blur:hover {
+  background-color: rgba(105, 238, 161, 0.5);
+  backdrop-filter: blur(8px);
+}
+.modal-active {
+  display: block;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  clip-path: circle(73.7% at 48% 51%);
+}
+
+.modal-inactive {
+  clip-path: circle(0.0% at 0 50%);
+}
+
+
+</style>
